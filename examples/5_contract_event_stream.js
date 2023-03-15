dc804f73e970306ccaf9df631d9c93ac3b1d5521
Nohyun Nehemiah Kwak <nehemiah.kwak@krustuniverse.com>
2023-03-15 14:49:38 +0900

    Update 5_contract_event_stream.js

--- examples/5_contract_event_stream.js
+++ examples/5_contract_event_stream.js
@@ -1,24 +1,18 @@
 const { ethers } = require("ethers");
+const fs = require('fs')
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.providers.JsonRpcProvider("https://api.baobab.klaytn.net:8651")
 
-const ERC20_ABI = [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
+const TOKEN_ADDRESS = '0xaa0A40391Df2eD1bd2A3FE3b9AC869619Cc4eD8e' 
 
-    "event Transfer(address indexed from, address indexed to, uint amount)"
-];
+const TOKEN_ABI = JSON.parse(fs.readFileSync('./contracts/ServiceChainToken.abi', 'utf8'));
 
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
+const contract = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, provider)
 
 const main = async () => {
     const block = await provider.getBlockNumber()
 
-    const transferEvents = await contract.queryFilter('Transfer', block - 1, block)
+    const transferEvents = await contract.queryFilter('Transfer', 0, block)
     console.log(transferEvents)
 }
 
