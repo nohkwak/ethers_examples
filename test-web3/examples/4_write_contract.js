27220d5549e39380b1c549f3a20dc852c2c9f2d2
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-03-20 15:47:11 +0900

    Update web3

--- test-web3/examples/4_write_contract.js
+++ test-web3/examples/4_write_contract.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const web3 = new Web3("https://api.baobab.klaytn.net:8651")
 
 const account1 = '0x3208ca99480f82bfe240ca6bc06110cd12bb6366' // sender address 
-const privateKey1 = '' // Private key of sender 
+const privateKey1 = fs.readFileSync('../privateKey', 'utf8') // Private key of sender 
 
 const account2 = '0xc40b6909eb7085590e1c26cb3becc25368e249e9' // reciever address 
 
